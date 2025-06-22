c5f942765538d51e5cc71e43c869b4534e18adb2
userRr423 <117460437+userRr423@users.noreply.github.com>
2025-06-22 21:29:28 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -34,7 +34,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         # "http://localhost:5173",
-        # "http://194.169.163.65/",
+        # "http://194.169.163.65",
         "https://site-test-deploy1.ru",
     ],
     allow_credentials=True,
